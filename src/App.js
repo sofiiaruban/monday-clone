a132606f9e3a1dec4330b9f0bee828da38c9980b
Sofia Ruban <sophiaruban249324@gmail.com>
2022-08-23 22:01:53 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Dashboard from "./pages/Dashboard";
+import TicketPage from "./pages/TicketPage";
+import CategoriesContext from "./context";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [categories, setCategories] = useState(null);
+  const value = { categories, setCategories };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <CategoriesContext.Provider value={value}>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Dashboard />}></Route>
+            <Route path="/ticket" element={<TicketPage />}></Route>
+            <Route
+              path="/ticket/:id"
+              element={<TicketPage editMode={true} />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </CategoriesContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/TicketСard.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import AvatarDisplay from "./AvatarDisplay";
+import StatusDisplay from "./StatusDisplay";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import DeleteBlock from "./DeleteBlock";
+
+const TicketCard = ({ color, ticket }) => {
+  return (
+    <div className="ticket-card">
+      <div className="ticket-color" style={{ backgroundColor: color }}></div>
+      <Link to={`/ticket/${ticket.documentId}`} id="link">
+        <h3>{ticket.title}</h3>
+        <AvatarDisplay ticket={ticket} />
+        <StatusDisplay status={ticket.status} />
+        <PriorityDisplay priority={Number(ticket.priority)} />
+        <ProgressDisplay progress={Number(ticket.progress)} />
+      </Link>
+      <DeleteBlock documentId={ticket.documentId} />
+    </div>
+  );
+};
+
+export default TicketCard;
